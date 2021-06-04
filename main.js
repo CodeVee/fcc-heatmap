dab7651ebe61004753054c21bd307965068b0e09
Victor Onyebuchi <victorchike247@gmail.com>
2021-06-04 16:39:59 +0100

    added header and colors

--- main.js
+++ main.js
@@ -11,25 +11,156 @@ const margin = {top: 80, right: 25, bottom: 30, left: 40},
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+const colorbrewer = {
+  RdYlBu: {
+    3: ['#fc8d59', '#ffffbf', '#91bfdb'],
+    4: ['#d7191c', '#fdae61', '#abd9e9', '#2c7bb6'],
+    5: ['#d7191c', '#fdae61', '#ffffbf', '#abd9e9', '#2c7bb6'],
+    6: ['#d73027', '#fc8d59', '#fee090', '#e0f3f8', '#91bfdb', '#4575b4'],
+    7: [
+      '#d73027',
+      '#fc8d59',
+      '#fee090',
+      '#ffffbf',
+      '#e0f3f8',
+      '#91bfdb',
+      '#4575b4'
+    ],
+    8: [
+      '#d73027',
+      '#f46d43',
+      '#fdae61',
+      '#fee090',
+      '#e0f3f8',
+      '#abd9e9',
+      '#74add1',
+      '#4575b4'
+    ],
+    9: [
+      '#d73027',
+      '#f46d43',
+      '#fdae61',
+      '#fee090',
+      '#ffffbf',
+      '#e0f3f8',
+      '#abd9e9',
+      '#74add1',
+      '#4575b4'
+    ],
+    10: [
+      '#a50026',
+      '#d73027',
+      '#f46d43',
+      '#fdae61',
+      '#fee090',
+      '#e0f3f8',
+      '#abd9e9',
+      '#74add1',
+      '#4575b4',
+      '#313695'
+    ],
+    11: [
+      '#a50026',
+      '#d73027',
+      '#f46d43',
+      '#fdae61',
+      '#fee090',
+      '#ffffbf',
+      '#e0f3f8',
+      '#abd9e9',
+      '#74add1',
+      '#4575b4',
+      '#313695'
+    ]
+  },
+  RdBu: {
+    3: ['#ef8a62', '#f7f7f7', '#67a9cf'],
+    4: ['#ca0020', '#f4a582', '#92c5de', '#0571b0'],
+    5: ['#ca0020', '#f4a582', '#f7f7f7', '#92c5de', '#0571b0'],
+    6: ['#b2182b', '#ef8a62', '#fddbc7', '#d1e5f0', '#67a9cf', '#2166ac'],
+    7: [
+      '#b2182b',
+      '#ef8a62',
+      '#fddbc7',
+      '#f7f7f7',
+      '#d1e5f0',
+      '#67a9cf',
+      '#2166ac'
+    ],
+    8: [
+      '#b2182b',
+      '#d6604d',
+      '#f4a582',
+      '#fddbc7',
+      '#d1e5f0',
+      '#92c5de',
+      '#4393c3',
+      '#2166ac'
+    ],
+    9: [
+      '#b2182b',
+      '#d6604d',
+      '#f4a582',
+      '#fddbc7',
+      '#f7f7f7',
+      '#d1e5f0',
+      '#92c5de',
+      '#4393c3',
+      '#2166ac'
+    ],
+    10: [
+      '#67001f',
+      '#b2182b',
+      '#d6604d',
+      '#f4a582',
+      '#fddbc7',
+      '#d1e5f0',
+      '#92c5de',
+      '#4393c3',
+      '#2166ac',
+      '#053061'
+    ],
+    11: [
+      '#67001f',
+      '#b2182b',
+      '#d6604d',
+      '#f4a582',
+      '#fddbc7',
+      '#f7f7f7',
+      '#d1e5f0',
+      '#92c5de',
+      '#4393c3',
+      '#2166ac',
+      '#053061'
+    ]
+  }
+};
+
+const projectName = 'heat-map';
 
 const processData = async () => {
 
     const res = await fetch(url);
     const data = await res.json();
 
-    const years = data.monthlyVariance.map(x => x.year);
-    const uniqueYears = [...new Set(years)]
-    console.log(uniqueYears)
+    data.monthlyVariance.forEach(val =>  val.month -= 1);
 
-    const x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(uniqueYears)
-    .padding(0.05);
-  svg.append("g")
-    .style("font-size", 15)
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(0))
-    .select(".domain").remove()
+    var section = d3.select('body').append('section');
+
+    const heading = section.append('heading');
+    heading
+      .append('h1')
+      .attr('id', 'title')
+      .text('Monthly Global Land-Surface Temperature');
+    heading
+      .append('h3')
+      .attr('id', 'description')
+      .html(
+        `${data.monthlyVariance[0].year} - 
+        ${data.monthlyVariance[data.monthlyVariance.length - 1].year}
+        : base temperature ${data.baseTemperature} &#8451;
+        `
+      );
 }
 
 processData();
